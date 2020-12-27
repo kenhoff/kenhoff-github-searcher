b0501390d760d17f0019781aa57a4cbdb4b8b91f
Ken Hoff <ken@hoff.tech>
2020-12-27 15:40:23 -0700

    ts conversion, app

--- App.ts
+++
@@ -1,4 +0,0 @@
-const App = () => {
-    return "hello app";
-};
-export default App;
---
+++ App.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+const App = () => {
+    return <h1>"hello app"</h1>;
+};
+export default App;
